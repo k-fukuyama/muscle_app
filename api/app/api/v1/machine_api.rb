18b6839189813519d8429dc2b6b71d4a42c55a1d
福山慶太 <fukuyamakeita@fukuyamakeitanoMacBook-Air.local>
2021-12-12 17:15:27 +0900

    筋トレ器具の一覧を取得できるようにした

--- api/app/api/v1/machine_api.rb
+++ api/app/api/v1/machine_api.rb
@@ -1,5 +1,9 @@
 class MachineApi < Grape::API
   resources :machine do
+    get do
+      present ({machines: Machine.all.select(:id, :name, :description)})
+    end
+
     post do
       Machine.create!(name: params[:machine][:name], description: params[:machine][:description])
 
--- front/src/components/muscle_machines/muscleMachinesIndex.vue
+++ front/src/components/muscle_machines/muscleMachinesIndex.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
-    <h1>筋トレ器具のページ</h1>
-    <machineForm />
+    <h1>筋トレ器具一覧</h1>
+    <ul v-for="machine in machines" :key="machine.id">
+      <li>{{machine.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import machineForm from './machineForm.vue'
+import axios from 'axios';
 
 export default {
-  components: {
-    machineForm
+
+  data: () => ({
+    machines: ''
+  }),
+
+  created() {
+    this.getMachines()
+  },
+
+  methods: {
+    getMachines() {
+      axios.get(`http://localhost:3000/api/v1/machine`).then((res) => {
+        this.machines = res.data['machines']
+      })
+    }
   }
 }
 </script>
